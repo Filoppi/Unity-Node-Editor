15c9c9d262a79518d3d599195e2e7caf26f7f655
Shane Lillie <ignignokterr@gmail.com>
2015-12-16 07:29:27 -0800

    Skeleton node editor

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Unity-Node-Editor
+
 Node-based editor
+
+
+* https://unity3d.com/learn/tutorials/modules/beginner/live-training-archive/state-machine-interface
+* http://forum.unity3d.com/threads/simple-node-editor.189230/
+  * useful for AI and sequencing
+* http://forum.unity3d.com/threads/flowcanvas-inspired-by-unreal-blueprints.245646/
+* http://forum.unity3d.com/threads/nodecanvas-behaviour-trees-state-machines-dialogue-trees.227190/
+* http://stackoverflow.com/questions/17593101/how-to-write-a-gui-editor-for-graph-or-tree-structures
+  * https://www.youtube.com/watch?v=gHTJmGGH92w
+  
---
+++ Unity-Node Editor/Assets/Scripts/Editor/NodeEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+using UnityEditor;
+using UnityEngine;
+
+namespace EnergonSoftware.Editor
+{
+    public class NodeEditor : EditorWindow
+    {
+        private class Node
+        {
+            public Rect Rect { get; private set; }
+            
+            public Node(Rect rect)
+            {
+                Rect = rect;
+            }
+            
+            public void Render()
+            {
+                Rect = GUI.Window(1, Rect, OnRender, "Test Node");
+            }
+            
+            private void OnRender(int id)
+            {
+                GUI.DragWindow();
+            }
+        }
+        
+        [MenuItem("Energon Software/Node Editor")]
+        public static void ShowEditor()
+        {
+            NodeEditor editor = EditorWindow.GetWindow<NodeEditor>();
+        }
+        
+        private readonly List<Node> _nodes = new List<Node>();
+        
+        private NodeEditor()
+        {
+            _nodes.Add(new Node(new Rect(10, 10, 100, 100)));
+        }
+        
+        private void OnGUI()
+        {
+            BeginWindows();
+                foreach(Node node in _nodes) {
+                    node.Render();
+                }
+            EndWindows();
+        }
+    }
+}
