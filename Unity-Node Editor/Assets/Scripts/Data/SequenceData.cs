91661fec9a9e751e7c231e1e348a31e07f875e3b
Shane Lillie <ignignokterr@gmail.com>
2016-01-14 20:13:53 -0800

    Adding a data scriptableobject

---
+++ Unity-Node Editor/Assets/Scripts/Data/SequenceData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace EnergonSoftware.Data
+{
+    [Serializable]
+    public class SequenceData : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [UnityEditor.MenuItem("Assets/Create/EnergonSoftware/Sequence Data")]
+        public static void CreateAsset()
+        {
+            Editor.ScriptableObjectUtil.CreateAsset<SequenceData>();
+        }
+#endif
+
+        public string sequenceName = string.Empty;
+
+        [Range(0.0f, 100.0f)]
+        public int priority = 0;
+    }
+}
---
+++ Unity-Node Editor/Assets/Scripts/Util/ScriptableObjectUtil.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+using UnityEngine;
+ 
+namespace EnergonSoftware.Editor
+{
+#if UNITY_EDITOR
+    // http://wiki.unity3d.com/index.php?title=CreateScriptableObjectAsset
+    public static class ScriptableObjectUtil
+    {
+        /// <summary>
+        //	This makes it easy to create, name and place unique new ScriptableObject asset files.
+        /// </summary>
+        public static void CreateAsset<T> () where T : ScriptableObject
+        {
+            T asset = ScriptableObject.CreateInstance<T>();
+ 
+            string path = UnityEditor.AssetDatabase.GetAssetPath(UnityEditor.Selection.activeObject);
+            if(string.IsNullOrEmpty(path)) {
+                path = "Assets";
+            } else if(!string.IsNullOrEmpty(Path.GetExtension(path))) {
+                string filename = Path.GetFileName(UnityEditor.AssetDatabase.GetAssetPath(UnityEditor.Selection.activeObject));
+                path = path.Replace(filename ?? string.Empty, string.Empty);
+            }
+ 
+            string assetPathAndName = UnityEditor.AssetDatabase.GenerateUniqueAssetPath(Path.Combine(path, $"New {typeof(T).Name}.asset"));
+            UnityEditor.AssetDatabase.CreateAsset(asset, assetPathAndName);
+ 
+            UnityEditor.AssetDatabase.SaveAssets();
+            UnityEditor.AssetDatabase.Refresh();
+
+            UnityEditor.EditorUtility.FocusProjectWindow();
+            UnityEditor.Selection.activeObject = asset;
+        }
+    }
+#endif
+}
